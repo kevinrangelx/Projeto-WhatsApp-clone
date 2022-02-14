7b31584a1e4735bb18bf63f0921561285bc21452
kevin <kevin.rangelpf@gmail.com>
2022-02-14 17:33:08 -0300

    7 - Configurando Eventos no Menu Anexar

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -117,34 +117,66 @@ class WhatsAppController {
                 });
             });
         });
-        this.el.btnAttach.on('click', e=> {
-            
+        this.el.btnAttach.on('click', e=> {            
             e.stopPropagation();
             this.el.menuAttach.addClass('open');
             document.addEventListener('click', this.closeMenuAttach.bind(this));
-
         });
         this.el.btnAttachPhoto.on('click', e=>{
-            console.log('photo');
+            this.el.inputPhoto.click();
+        });
+        this.el.inputPhoto.on('change', e=>{
+            console.log(this.el.inputPhoto.files);
+            [...this.el.inputPhoto.files].forEach(file=>{
+                console.log(file);
+            });
         });
         this.el.btnAttachCamera.on('click', e=>{
-            console.log('camera');
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height':'calc(100%-120px)'
+            });
+        });
+        this.el.btnClosePanelCamera.on('click', e=>{
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+        this.el.btnTakePicture.on('click', e =>{
+            console.log('take picture');
         });
         this.el.btnAttachDocument.on('click', e=>{
-            console.log('document');
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height':'calc(100%-120px)'
+            });
+        });
+        this.el.btnClosePanelDocumentPreview.on('click', e=>{
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+        this.el.btnSendDocument.on('click', e=> {
+            console.log('send document');
         });
         this.el.btnAttachContact.on('click', e=>{
-            console.log('contact');
+            this.el.modalContacts.show();
+        });
+        this.el.btnCloseModalContacts.on('click', e=>{
+            this.el.modalContacts.hide();
         });
     }
+    closeAllMainPanel(){
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+    }
     closeMenuAttach(e){
-
         document.removeEventListener('click', this.closemenuAttach);
         this.el.menuAttach.removeClass('open');
     }
     closeAllLeftPanel(){
         this.el.panelAddContact.hide();
         this.el.panelEditProfile.hide();
-
     }
 }
